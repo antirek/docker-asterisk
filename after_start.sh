61310870f6389770d0cf5da29ccc28119bbd3e27
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-11-16 12:24:42 +0700

    add rm file

--- after_start.sh
+++ after_start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-safe_asterisk
 
 ln -s /tmp/lua-dialplan/ /usr/local/lib/lua/5.1/dialplan
 
+rm -f -v /etc/asterisk/extensions.lua
 ln -s /tmp/lua-dialplan/extensions.lua /etc/asterisk/extensions.lua
+
+asterisk
 
 sleep 1
 
