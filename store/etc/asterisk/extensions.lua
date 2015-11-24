a7093054ff6c3f78143d832bb3a3894a28d04ce0
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-11-24 17:28:04 +0700

    add sample of lua dialplan

--- store/etc/asterisk/extensions.lua
+++ store/etc/asterisk/extensions.lua
@@ -4,10 +4,18 @@ extensions = {
 
 		["_NXXXXXX"] = outgoing_local;
 
-		["*12"] = function()
+		["*12"] = function ()
 			app.sayunixtime();
 		end;
 
+		["_1XX"] = function (context, extension) 
+			app.noop("context: "..context..", extension: "..extension);
+			app.dial('SIP/'..extension);
+		end;
+
+		["200"] = function (context, extension)
+			app.playback("/var/menu/demo");
+		end
 	};
 };
 
