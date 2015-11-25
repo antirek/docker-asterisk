cbc982b0e4dfbcf440b844671ad77556600b2350
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-11-25 12:42:55 +0700

    add ivr choose

--- store/etc/asterisk/extensions.lua
+++ store/etc/asterisk/extensions.lua
@@ -1,22 +1,30 @@
 
 extensions = {
-	["internal"] = {
+    ["internal"] = {
 
-		["_NXXXXXX"] = outgoing_local;
+        ["_NXXXXXX"] = outgoing_local;
 
-		["*12"] = function ()
-			app.sayunixtime();
-		end;
+        ["*12"] = function ()
+            app.sayunixtime();
+        end;
 
-		["_1XX"] = function (context, extension) 
-			app.noop("context: "..context..", extension: "..extension);
-			app.dial('SIP/'..extension);
-		end;
+        ["_1XX"] = function (context, extension) 
+            app.noop("context: "..context..", extension: "..extension);
+            app.dial('SIP/'..extension);
+        end;
 
-		["200"] = function (context, extension)
-			app.playback("/var/menu/demo");
-		end;
-	};
+        ["200"] = function (context, extension)
+            app.read("IVR_CHOOSE", "/var/menu/demo", 1, nil, 2, 3);
+            local choose = channel["IVR_CHOOSE"]:get();
+            if choose == '1' then
+                app.dial('SIP/120');
+            elseif choose == '2' then
+                app.dial('Local/100@internal');
+            else
+                app.hangup();
+            end;
+        end;
+    };
 };
 
 hints = {};
