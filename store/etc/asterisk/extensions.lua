a25f6bb231cce9d82947fc4adb855980b540e4a9
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-11-25 14:14:12 +0700

    fix dialplan

--- store/etc/asterisk/extensions.lua
+++ store/etc/asterisk/extensions.lua
@@ -1,29 +1,46 @@
 
+local dial = function (context, extension)
+    app.noop("context: " .. context .. ", extension: " .. extension);
+    app.dial('SIP/' .. extension, 10);
+    
+    local dialstatus = channel["DIALSTATUS"]:get();
+    app.noop('dialstatus: '..dialstatus);
+    app.set("CHANNEL(language)=ru");
+
+    if dialstatus == 'BUSY' then
+        app.playback("followme/sorry");        
+    elseif dialstatus == 'CHANUNAVAIL' then 
+        app.playback("followme/sorry");
+    end;
+
+    app.hangup();
+end;
+
+local ivr = function (context, extension)        
+    app.read("IVR_CHOOSE", "/var/menu/demo", 1, nil, 2, 3);
+    local choose = channel["IVR_CHOOSE"]:get();
+
+    if choose == '1' then
+        app.queue('1234');
+    elseif choose == '2' then
+        dial('internal', '101');
+    else
+        app.hangup();
+    end;
+end;
+
 extensions = {
     ["internal"] = {
-
-        ["_NXXXXXX"] = outgoing_local;
 
         ["*12"] = function ()
             app.sayunixtime();
         end;
 
-        ["_1XX"] = function (context, extension) 
-            app.noop("context: "..context..", extension: "..extension);
-            app.dial('SIP/'..extension);
-        end;
+        ["_1XX"] = dial;
 
-        ["200"] = function (context, extension)
-            app.read("IVR_CHOOSE", "/var/menu/demo", 1, nil, 2, 3);
-            local choose = channel["IVR_CHOOSE"]:get();
-            if choose == '1' then
-                app.dial('SIP/120');
-            elseif choose == '2' then
-                app.dial('Local/100@internal');
-            else
-                app.hangup();
-            end;
-        end;
+        ["200"] = ivr;
+
+        ["_NXXXXXX"] = outgoing_route_function;
     };
 };
 
